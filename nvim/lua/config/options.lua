83a367410d63766292afdf7e4a3ae7667f725cce
Cody Stubbs <cody@mediusag.com>
2024-12-19 16:43:11 -0500

    Start nvim from cleanslate

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
 vim.g.mapleader = " "
 vim.g.autoformat = false
 vim.g.lazyvim_picker = "telescope"
@@ -18,7 +21,6 @@ vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
 vim.opt.expandtab = true
 vim.opt.scrolloff = 10
--- vim.opt.shell = "fish"
 vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
 vim.opt.inccommand = "split"
 vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
@@ -36,15 +38,15 @@ vim.opt.splitkeep = "cursor"
 vim.opt.mouse = ""
 
 -- Undercurl
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
+-- vim.cmd([[let &t_Cs = "\e[4:3m"]])
+-- vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
 -- Add asterisks in block comments
-vim.opt.formatoptions:append({ "r" })
-
-vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
-vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
-
-if vim.fn.has("nvim-0.8") == 1 then
-	vim.opt.cmdheight = 0
-end
+-- vim.opt.formatoptions:append({ "r" })
+--
+-- vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
+-- vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
+--
+-- if vim.fn.has("nvim-0.8") == 1 then
+--   vim.opt.cmdheight = 0
+-- end
